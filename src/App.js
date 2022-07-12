d340e93cdb944b5ca08af04b1b01161ba0418811
tushar <tushargupta010@gmail.com>
2022-07-12 13:38:52 +0530

    useEffect hook and localStorage is added

--- src/App.js
+++ src/App.js
@@ -3,10 +3,17 @@ import "./App.css";
 import Header from "./MyComponents/Header";
 import { Todos } from "./MyComponents/Todos";
 import { Footer } from "./MyComponents/Footer";
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { AddTodo } from "./MyComponents/AddTodo";
 
 function App() {
+  let initTodos;
+  if (localStorage.getItem("todos") === null) {
+    initTodos = [];
+  } else {
+    initTodos = JSON.parse(localStorage.getItem("todos"));
+  }
+
   const onDelete = (todo) => {
     console.log("I am onDelete todo : ", todo);
     // Deleting this way in reactb doesn;t work
@@ -24,23 +31,10 @@ function App() {
   // Declare a new state variable, which we'll call "count"
   // const [count, setCount] = useState(0);
 
-  const [todos, setTodos] = useState([
-    {
-      sno: 1,
-      title: "Sunday",
-      description: "Funday",
-    },
-    {
-      sno: 2,
-      title: "Monday",
-      description: "Oneday",
-    },
-    {
-      sno: 3,
-      title: "Tuesday",
-      description: "Twoday",
-    },
-  ]);
+  const [todos, setTodos] = useState(initTodos);
+  useEffect(() => {
+    localStorage.setItem("todos", JSON.stringify(todos));
+  }, [todos]);
 
   const addTodo = (title, description) => {
     let sno = 0;
